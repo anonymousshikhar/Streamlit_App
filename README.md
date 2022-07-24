facbd27112315b6644774152d7d2b9b7b9c6e0fd
Akshay Shikhar <shikhar.rock07@gmail.com>
2022-07-24 17:12:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### This is a demo for Streamlit application to be deployed on Heroku, a free web hosting service.
---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.header('''
+This is an Streamlit app Demo''')
+
+st.subheader('Use case: Addition of Two numbers')
+
+def user_input():
+  first_num = st.number_input('Enter First Number')
+  second_num = st.number_input('Enter Second Number')
+
+  data = {'First Number':first_num,
+          'Second Number':second_num}
+
+  features = pd.DataFrame(data, index = [0])
+  features['Sum of the numbers'] = features.sum(axis = 1)
+  return features 
+
+df = user_input()
+st.write(df)
